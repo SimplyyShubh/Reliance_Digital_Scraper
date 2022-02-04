17740ea5268a605a73258fabe2e83b05812f954e
SimplyyShubh <simplyy11@gmail.com>
2022-02-05 01:41:25 +0530

    Added If Out Of Stock

--- app.py
+++ app.py
@@ -12,20 +12,57 @@ from csv import writer
 import json
 
 
-# options = Options()
-# options.headless = True
+options = Options()
+options.headless = True
+options.add_argument("--disable-infobars")
 
+options.add_argument("start-maximized")
 
-driver = webdriver.Firefox()
+options.add_argument("--disable-extensions")
 
-def Scraper():
+# options.add_experimental_option("prefs",
+#                                {"profile.default_content_setting_values.notifications": 2
+#                                 })
+# options.add_Argument("--disable-notifications")
+
+driver = webdriver.Firefox(options=options)
+link = "https://www.reliancedigital.in/asus-hx019t-rog-strix-g17-gaming-laptop-amd-ryzen-7-4800h-8gb-512gb-ssd-4-gb-nvidia-geforce-gtx-1650-windows-10-fhd-43-94-cm-17-3-inch-/p/491997666"
+driver.get(link)
+
+def getPrice():
     try:
-        link = "https://www.reliancedigital.in/asus-hz071ts-rog-zephyrus-g14-gaming-laptop-amd-ryzen-7-5800hs-8gb-512gb-ssd-4gb-nvidia-geforce-gtx-1650-graphics-windows-10-mso-full-hd-35-56-cm-14-inch-/p/492573909"
-        driver.get(link)
         price = driver.find_element(By.XPATH, '//*[@class="pdp__offerPrice"]/span[2]').text
-        print("₹" + price)
     except:
         price = "Error"
+
     return price
     
-print(Scraper())
+def closeAlert():
+    try:
+        driver.find_element(By.XPATH, '//*[@id="wzrk-cancel"]').click()
+    except:
+        # print("No Alert")
+        pass
+
+def isOOS():
+    #Check If Product is Out Of Stock
+    try:
+        closeAlert()
+        OOS = len(driver.find_elements(By.CLASS_NAME, 'pdp__noStockBlock')) > 0
+    except:
+        OOS = False
+    return OOS
+
+def runEndlessly():
+
+    while True:
+        driver.get(link)
+        price = getPrice()
+        iOOS = isOOS()
+        if iOOS:
+            print("Currently OOS")
+
+        print("Current Price=> " , price)
+        time.sleep(2)
+
+# runEndlessly()
